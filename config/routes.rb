574c17c83f2d2bdb81715c180393a353f8af0255
Rodman Aguirre <rodfagui@gmail.com>
2017-07-24 16:34:02 -0500

    Change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :products
 
-  namespace :api, defaults: { format: "json" } do
+  namespace :api do
     namespace :v1 do
       resources :products
     end
